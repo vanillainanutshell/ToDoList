66c0f2375102bd2df1ed85661d0b34ceb41f2639
Vanillainanutshell <maurovasconcelos11@gmail.com>
2022-01-19 11:59:34 -0200

    Add render function and tasklist array

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,47 @@
 import _ from 'lodash';
 
-function component() {
-    const element = document.createElement('div');
+let tasklist = [{
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+        description: 'Task 1',
+        completed: false,
+        index: 0,
+    },
+    {
+        description: 'Task 2',
+        completed: false,
+        index: 1,
+    },
+    {
+        description: 'Task 3',
+        completed: false,
+        index: 2,
+    },
+    {
+        description: 'Task 4',
+        completed: false,
+        index: 3,
+    }
+];
 
-    return element;
-}
+const render = () => {
+    tasklist = tasklist.sort((a, b) => a.index - b.index);
+    console.log(tasklist);
+};
 
-document.body.appendChild(component());
+window.onload = () => {
+    render()
+};
+
+// for (let i = 0; i < taskList.length; i += 1) {
+//     taskUl.innerHTML = `
+//     <li>
+//     </li>
+// `;
+// }
+// class Task {
+//     constructor(description, index, status) {
+//       this.description = description;
+//       this.completed = status;
+//       this.index = index;
+//     }
+//   }
