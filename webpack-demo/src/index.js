c2f2fef532b7d5d94a56187e05836b42d9b61dee
Vanillainanutshell <maurovasconcelos11@gmail.com>
2022-01-19 15:09:59 -0200

    Add linters

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,41 +1,41 @@
 import './style.css';
 
-let taskList = [{
+const taskList = [{
 
-        description: 'Task 1',
-        completed: false,
-        index: 0,
-    },
-    {
-        description: 'Task 2',
-        completed: false,
-        index: 1,
-    },
-    {
-        description: 'Task 3',
-        completed: false,
-        index: 2,
-    },
-    {
-        description: 'Task 4',
-        completed: false,
-        index: 3,
-    }
+  description: 'Task 1',
+  completed: false,
+  index: 0,
+},
+{
+  description: 'Task 2',
+  completed: false,
+  index: 1,
+},
+{
+  description: 'Task 3',
+  completed: false,
+  index: 2,
+},
+{
+  description: 'Task 4',
+  completed: false,
+  index: 3,
+},
 ];
 const generateTask = () => {
-    let taskUl = document.getElementById('taskUl');
-    for (let i = 0; i < taskList.length; i += 1) {
-        const li = document.createElement('li');
-        li.innerHTML = `
+  const taskUl = document.getElementById('taskUl');
+  for (let i = 0; i < taskList.length; i += 1) {
+    const li = document.createElement('li');
+    li.innerHTML = `
         <div>       
             <input type="checkbox" id="task" name="tasks">
             <label for="tasks">${taskList[i].description}</label>
         </div>
         <div><i class="fas fa-ellipsis-v"></i></i></div>`;
-        taskUl.appendChild(li);
-    };
+    taskUl.appendChild(li);
+  }
 };
 
 window.onload = () => {
-    generateTask()
+  generateTask();
 };
